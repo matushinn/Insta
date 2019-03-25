70b410ec0d8f852447d0c1563e83c4e7eb25e101
shotaro-ooe <shoooe97@gmail.com>
2019-03-26 01:10:32 +0900

    first commit

---
+++ InstaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaApp
+//
+//  Created by 大江祥太郎 on 2019/03/26.
+//  Copyright © 2019 shotaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
